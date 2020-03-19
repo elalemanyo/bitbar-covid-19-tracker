87399e5f71037cad12c1484e3d74bafaad5cf1e4
victor <victor@elalemanyo.de>
2020-03-19 11:28:36 +0100

    improve script adding submenus for some countries

--- covid-tracker.1h.js
+++ covid-tracker.1h.js
@@ -11,47 +11,92 @@
 
 const https = require('https');
 const urls = [
-  "https://coronavirus-tracker-api.herokuapp.com/v2/latest",
-  "https://coronavirus-tracker-api.herokuapp.com/v2/locations/11",
-  "https://coronavirus-tracker-api.herokuapp.com/v2/locations/18",
+    'https://coronavirus-tracker-api.herokuapp.com/v2/latest',
+    'https://coronavirus-tracker-api.herokuapp.com/v2/locations/11',
+    'https://coronavirus-tracker-api.herokuapp.com/v2/locations/18',
+    'https://coronavirus-tracker-api.herokuapp.com/v2/locations/16',
+    'https://coronavirus-tracker-api.herokuapp.com/v2/locations/154'
 ];
 
-https.get(urls[0], res => {
-    let data = '';
-    res.on('data', chunk => {
-      data += chunk;
+const icon = 'iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAAEhyb7BAAAAAXNSR0IArs4c6QAAAdlJREFUOBGFkzFLHFEQgFeFeBowalAJnB4kIFpIuN+QcEmRziJ10mhpGkGwtEkgfQqv8RdYCkqKgHiNZcQmVa46MCoREaIx3/d8b9nDjQ58O/PmzZudfTObZTfySNUDGp+gAkE2kqFu9PG4ho6xz+BcI4k7U/BARwOq8BnceAJdUiuullkYlY1E4yw59P6G1xCOPMZoQak8xGuKv+DJTfBAEDeVY3BzAUx9CXlGT1qNm6kI3+2BIBZhWh1/otbW/x5KZRhvs3QnOivoCzCTJQTpTUbUH9D9MAhfIQ/EzpZgDn7ARzCT3VGHVy9i7EWHzqfRfht1HZ2NxsU4ej3a81G/QwdZ4WmGL3AAY3AEv2AWgpzyNKhIm/XLsFt4GGjG57AKtwLw3Sl+2DZY+y6YzOE0cd587P+KV/cTirdzxbpYui03YQ1KZQiv978GHvwG3uIheFGTkBJaoT9ILrbfCtwwgU1MwQamVnzHnijseWdBHDDvIM1QqsBJMIHN3gf9HbBlW3HdQnvxbyCMipdooHcwAH5CqiYlcCimwb7ra8MJvIBc7IJDasAOVMGZ9BMcu1SB+w6Pf2ZXAtZBUhvtQkrooYSf4HhawQzcK7UY8QrdjHYdHe4grkvVP0xXjBIOIZS9AAAAAElFTkSuQmCC';
+
+let responses = {},
+    completed_requests = 0,
+    urlsLength = urls.length;
+
+for (let i = 0; i < urlsLength; i++) {
+    https.get(urls[i], function(res) {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            responses[i] = JSON.parse(data);
+            completed_requests++;
+            if (completed_requests == urls.length) {
+                renderBitBarOutput(responses);
+            }
+        });
+    }).on('error', (e) => {
+        // console.log('Error: ' + e.message);
     });
-    res.on('end', () => {
-      let apiData = JSON.parse(data);
-      let icon = 'iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAAEhyb7BAAAAAXNSR0IArs4c6QAAAdlJREFUOBGFkzFLHFEQgFeFeBowalAJnB4kIFpIuN+QcEmRziJ10mhpGkGwtEkgfQqv8RdYCkqKgHiNZcQmVa46MCoREaIx3/d8b9nDjQ58O/PmzZudfTObZTfySNUDGp+gAkE2kqFu9PG4ho6xz+BcI4k7U/BARwOq8BnceAJdUiuullkYlY1E4yw59P6G1xCOPMZoQak8xGuKv+DJTfBAEDeVY3BzAUx9CXlGT1qNm6kI3+2BIBZhWh1/otbW/x5KZRhvs3QnOivoCzCTJQTpTUbUH9D9MAhfIQ/EzpZgDn7ARzCT3VGHVy9i7EWHzqfRfht1HZ2NxsU4ej3a81G/QwdZ4WmGL3AAY3AEv2AWgpzyNKhIm/XLsFt4GGjG57AKtwLw3Sl+2DZY+y6YzOE0cd587P+KV/cTirdzxbpYui03YQ1KZQiv978GHvwG3uIheFGTkBJaoT9ILrbfCtwwgU1MwQamVnzHnijseWdBHDDvIM1QqsBJMIHN3gf9HbBlW3HdQnvxbyCMipdooHcwAH5CqiYlcCimwb7ra8MJvIBc7IJDasAOVMGZ9BMcu1SB+w6Pf2ZXAtZBUhvtQkrooYSf4HhawQzcK7UY8QrdjHYdHe4grkvVP0xXjBIOIZS9AAAAAElFTkSuQmCC';
-      printBitBarLine(formatNumber(apiData.latest.confirmed), ['color=red', 'templateImage=' + icon]);
+}
 
-      printBitBarLine('---')
+function renderBitBarOutput(responses) {
+    if (Object.keys(responses).length == 0) {
+        printBitBarLine('⚠️', ['templateImage=' + icon]);
+    }
 
-      printBitBarLine('Confirmed: ' + formatNumber(apiData.latest.confirmed), ['color=red']);
-      printBitBarLine('Deaths: ' + formatNumber(apiData.latest.deaths), ['color=#000000']);
-      printBitBarLine('Recovered: ' + formatNumber(apiData.latest.recovered), ['color=#61B329']);
+    else {
+        printBitBarLine(formatNumber(responses[0].latest.confirmed), ['color=red', 'templateImage=' + icon]);
 
-      printBitBarLine('---');
+        printBitBarLine('---')
 
-      printBitBarLine("Dashboard", ['href=https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6']);
-      printBitBarLine('Source', ['href=https://github.com/CSSEGISandData/COVID-19']);
-    });
-  })
-  .on('error', err => {
-    console.log('Error: ' + err.message);
-  });
+        printBitBarLine('Confirmed: ' + formatNumber(responses[0].latest.confirmed), ['color=red']);
+        printBitBarLine('Deaths: ' + formatNumber(responses[0].latest.deaths), ['color=#000000']);
+        printBitBarLine('Recovered: ' + formatNumber(responses[0].latest.recovered), ['color=#61B329']);
+
+        printBitBarLine('---');
+
+        renderCountriesSubmenus(responses);
+    }
+
+    renderFooter();
+
+};
+
+function renderCountriesSubmenus(responses) {
+    let keys = Object.keys(responses),
+        country;
+
+    keys.shift();
+    for (let key of keys) {
+        country = responses[key].location;
+        printBitBarLine(country.country + ' (' + formatNumber(country.latest.confirmed) + ')');
+
+        printBitBarLine('--Confirmed: ' + formatNumber(country.latest.confirmed), ['color=red']);
+        printBitBarLine('--Deaths: ' + formatNumber(country.latest.deaths), ['color=#000000']);
+        printBitBarLine('--Recovered: ' + formatNumber(country.latest.recovered), ['color=#61B329']);
+    }
+};
+
+function renderFooter() {
+    printBitBarLine('---');
+
+    printBitBarLine('Dashboard', ['href=https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6']);
+    printBitBarLine('Source', ['href=https://github.com/CSSEGISandData/COVID-19']);
+}
 
 function formatNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-}
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+};
 
 function printBitBarLine(title, args) {
-  let lineArgs = [];
+    let lineArgs = [];
 
-  for (let i in args) {
-    lineArgs.push(args[i]);
-  }
+    for (let i in args) {
+        lineArgs.push(args[i]);
+    }
 
-  console.log((lineArgs.length !== 0)? title + '|' + lineArgs.join(' ') : title);
-}
+    console.log((lineArgs.length !== 0)? title + '|' + lineArgs.join(' ') : title);
+};
